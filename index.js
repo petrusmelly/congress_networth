c81707fe22655f95845460fd67effac308e0a430
Chris P <chris.a.petruccelli@gmail.com>
2025-06-03 17:03:56 -0500

    Started with mapbox. Il district polys added w/GeoJSON

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+// import mapboxgl from 'mapbox-gl'; // or 
+// import 'dotenv/config';
+
+require('dotenv').config();
 
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// virtual path prefix
+app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home', {token: process.env.MAPBOX_TOKEN});
 })
 
 app.listen(port, () => {
